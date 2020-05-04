f37043a8a399f70a69ccb707d4090714aa3923b2
Kel <kelstalker@hotmail.com>
2020-05-03 21:02:40 -0600

    added new screenshot of SpaceX API Launch Data request in Postman

--- includes/navigation.php
+++ includes/navigation.php
@@ -7,9 +7,9 @@
         <li>
             <a href="../includes/datatypes.php">Data Types</a>
         </li>
-        <li>
+        <!-- <li>
             <a href="../api-request-test1.php">API Request Test1 (JS and Endpoint)</a>
-        </li>
+        </li> -->
         <li>
             <a href="../api-external-spacex-launchdata.php">API Request for SpaceX Launch Info</a>
         </li>
