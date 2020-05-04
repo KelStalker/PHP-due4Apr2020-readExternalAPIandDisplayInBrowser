0255327d55d91033d3ac9556f0ef18f4e1d49cfc
Kel <kelstalker@hotmail.com>
2020-05-03 20:27:38 -0600

    Added API Postman screengrab for SpaceX launch data

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-alert( 'Hello Kel!');
+// Grab the DOM section we created in the api-request-test1 page
+const apiTest1Element = document.getElementById( 'api-test1');
+
+
+
+//  alert( 'Hello Kel!');   **WORKING!**
+
+// Sets the request URL and converts the response string to an object
+
+fetch( 'http://localhost:90/api/apidata.php' )
+    .then( Response => Response.json() )
+    // alreaded tested in POSTMAN, so can now use the data (in an OBJECT)
+    .then( apidata => {
+        const heading = document.createElement('h2');
+        heading.textContent = 'apidata';
+        const list = document.createElement( 'UL');
+        for ( const listItem in apidata ) {
+         listItem.innerHTML = `
+            <dl>
+                <dt>Age</dt>
+                <dd>${listItem.age}</dd>
+                <dt>Type</dt>
+                <dd>${listItem.type}</dd>
+                <dt>Age</dt>
+                <dd>${listItem.age}</dd>
+            </dl>
+         `;
+        }
+
+    } )
