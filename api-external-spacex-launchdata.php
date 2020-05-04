9379d6cc517d47f81de4b3aeb45518318cc0cc8c
Kel <kelstalker@hotmail.com>
2020-05-03 21:57:10 -0600

    datalist populates with flight info as I type, (lines20-32)  but editor still showing error on  line3

--- api-external-spacex-launchdata.php
+++ api-external-spacex-launchdata.php
@@ -17,4 +17,18 @@ $spaceXLaunchDataJSONString = file_get_contents( 'https://api.spacexdata.com/v3/
   <h1>External API PHP SpaceX Launch Data</h1>
   <?php include './includes/navigation.php'; ?>
   <h2>SpaceX Launch Info (2nd entry in array)</h2>
+    <dl>
+      <dt>Flight Info</dt>
+        <dd>
+          <?php echo $spaceXLaunchData->flight_number;?>
+        </dd>
+
+        <dd>
+          <?php echo $spaceXLaunchData->mission_name; ?>
+        </dd>
+
+        <dd>
+          <?php echo $spaceXLaunchData->launch_year;?>
+        </dd>
+    </dl>
   </body>
